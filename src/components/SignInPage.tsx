5d80d391836a1ce37a8c51212791c1ed000503be
LMBraca <102985211+LMBraca@users.noreply.github.com>
2024-09-08 19:05:46 -0700

    deploy

--- src/components/SignInPage.tsx
+++ src/components/SignInPage.tsx
@@ -31,7 +31,7 @@ const SignInPage: React.FC = () => {
       <h2>Sign In</h2>
       <form onSubmit={handleSignIn}>
         <div className="form-group">
-          <label htmlFor="username">Username:</label>
+          <label htmlFor="username">Usernaame:</label>
           <input
             type="text"
             id="username"
